6e332cae2f9168f8ed7601e960d8ca33eca439af
Muse <muse@local>
2026-09-08 20:35:17 -0400

    P13 fix: stable hazard names under Level, tests use Level/ paths

--- tests/test_p5.gd
+++ tests/test_p5.gd
@@ -22,7 +22,7 @@ func _run() -> void:
 	_gecko = scene.get_node("Gecko")
 	# Park every hazard: these suites test wall movement, not dodging.
 	for hn in ["FootstepA", "FootstepB", "Bicycle", "BackingCar"]:
-		var hz: Area3D = scene.get_node(hn)
+		var hz: Area3D = scene.get_node("Level/" + hn)
 		hz.set_physics_process(false)
 		hz.position = Vector3(100, 0, 100)
 	# Let _ready() hooks run and the physics settle. The gecko spawns a hair
